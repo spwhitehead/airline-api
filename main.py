158d6b33c260fe0574f95e3abc83a6b4a99229c1
Greg Anderson <greg.anderson.cs@gmail.com>
2024-04-15 07:17:12 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+Have these endpoints:
+
+GET / -> list[airline_name]
+GET /:airline_name -> list[flight_num]
+GET /:airline_name/:flight_num -> Flight
+
+POST /:airline
+PUT /:airline/:flight_num
+DELETE /:airline/:flight_num
+
+"""
