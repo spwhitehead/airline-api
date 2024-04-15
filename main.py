8ab564cb736665ad002165fffccd109730460755
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-04-15 12:23:36 -0600

    Not working -- commit to save progress

--- main.py
+++ main.py
@@ -10,24 +10,50 @@ PUT /:airline/:flight_num
 DELETE /:airline/:flight_num
 
 """
-import uvicorn
+import json
+import requests
 from fastapi import FastAPI
+from typing import Dict
 
 from models import Company, FlightData
 
 app = FastAPI()
 
+with open("airlines.json", "r") as f:
+    airline_list = json.load(f)
+
+airlines: dict[Company, FlightData] = {}
+
+
+def load_json_data():
+    try:
+        with open("airlines.json", "r") as f:
+            airline_list = json.load(f)
+        for airline_data in airline_list:
+            airline = Company.model_validate(airline_data)
+            airlines[airline.name] = airline
+    except Exception as e:
+        print(f"Failed to load airlines form JSON: {e}")
+
+
+load_json_data()
+
 
 @app.get("/airline_name")
 async def list_airline_name() -> list:
-    pass
+    return airlines.values()
 
 
 @app.get("{airline_name}/flight_num")
-async def list_flight_num() -> list:
+async def list_flight_num(airline_name: str) -> list:
     pass
 
 
 @app.get("{airline_name}/{flight_num}/flight_data")
 async def flight_data() -> FlightData:
+    pass
+
+
+@app.post("airline")
+async def create_airline():
     pass
